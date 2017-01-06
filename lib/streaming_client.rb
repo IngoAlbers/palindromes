a7ad4b905f098af85068cdc7df5e465ae0742a86
Ingo Albers <pjotrpansen@gmail.com>
2017-01-06 16:45:08 +0100

    Filter the tweet text to its essence

--- lib/streaming_client.rb
+++ lib/streaming_client.rb
@@ -20,8 +20,16 @@ class StreamingClient < TwitterClient
 
   def stream(min_length)
     client.sample do |object|
-      next unless object.is_a?(Twitter::Tweet)
-      rest_client.client.retweet(object) if object.text == object.text.reverse
+      # Check only english tweets
+      next unless object.is_a?(Twitter::Tweet) && object.lang == 'en'
+
+      # Remove hashtags and mentions and filter only letters
+      essence = object.text.downcase.gsub(/\B[@#]\S+\b/, '').gsub(/[^a-z]/, '')
+
+      # Skip palindromes that are too short (single letters, etc.)
+      next if essence.length < min_length.to_i
+
+      rest_client.client.retweet(object) if essence == essence.reverse
     end
   end
 end
